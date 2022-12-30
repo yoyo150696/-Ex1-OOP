bbdf070269e05dc9fdf038ee04995bd250f24455
davidy1995 <84707578+davidy1995@users.noreply.github.com>
2022-12-30 13:22:36 +0200

    Add files via upload

---
+++ src/main/java/GroupAdmin.java
@@ -0,0 +1,52 @@
+import observer.Member;
+import observer.Sender;
+import observer.UndoableStringBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroupAdmin implements Sender {
+    private List<Member> mems = new ArrayList<>();
+    private UndoableStringBuilder us = new UndoableStringBuilder();
+
+    @Override
+    public void register(Member obj) {
+        this.mems.add(obj);
+        obj.update(us);
+    }
+
+    @Override
+    public void unregister(Member obj) {
+        this.mems.remove(obj);
+    }
+    //
+    @Override
+    public void insert(int offset, String obj) {
+        us.insert(offset,obj);
+    }
+    //
+    @Override
+    public void append(String obj) {
+        us.append(obj);
+    }
+
+    @Override
+    public void delete(int start, int end) {
+        us.delete(start, end);
+    }
+
+    @Override
+    public void undo() {
+        us.undo();
+    }
+
+
+
+
+    public UndoableStringBuilder getUdoableStringBuilder() {
+        return this.us;
+    }
+    public List<Member> getAllMembers() {
+        return mems;
+    }
+}
